928e6ea9c7b56b20ee5b6f6005260893b8738de2
Sergey <go.for.broke1006@gmail.com>
2018-12-06 12:28:00 +0300

    Create music.h

---
+++ music.h
@@ -0,0 +1,21 @@
+#ifndef AUDIO_GN_MUSIC_H
+#define AUDIO_GN_MUSIC_H
+
+class GuitarConstants {
+public:
+    static const double e;
+    static const double A;
+    static const double D;
+    static const double G;
+    static const double B;
+    static const double E;
+}
+
+const double GuitarConstants::e = 82.407;
+const double GuitarConstants::A = 110.000;
+const double GuitarConstants::D = 146.832;
+const double GuitarConstants::G = 195.998;
+const double GuitarConstants::B = 246.942;
+const double GuitarConstants::E = 329.628;
+
+#endif //AUDIO_GN_MUSIC_H
