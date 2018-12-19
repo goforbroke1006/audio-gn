0fce78bce53412789c620edb226ede428bd2371d
GoForBroke <go.for.broke1006@gmail.com>
2018-12-19 10:47:39 +0300

    update algorithm

--- main.cpp
+++ main.cpp
@@ -12,30 +12,41 @@ int main() {
 
     ofstream file("result.wav", ios::binary);
 
-    double hz = 44100;
+//    double hz = 44100;
+    double hz = 96000;
     unsigned long duration = 20; // 20 seconds
     unsigned short channelCount = 2; // stereo
-    unsigned short bitsPerSample = 8;
+    unsigned short bitsPerSample = 16;
     unsigned int subchunk2Size = 0;
     const WAVHEADER &wavheader = createWavHeader(hz, duration, channelCount, bitsPerSample, subchunk2Size);
 
     file.write(reinterpret_cast<const char *>(&wavheader), sizeof(wavheader));
 
     constexpr double two_pi = 6.283185307179586476925286766559;
-    constexpr double max_amplitude = 32760.0 / 2;
+    constexpr double max_amplitude = 32760.0;
 //    double frequency = 261.626; // musical note "C"
 //    double frequency = 293.6648; // musical note "D"
     double frequency = Piano::KEY_42;
 
-    for (int n = 0; n < subchunk2Size; ++n) {
-        double amplitude = (double) n / subchunk2Size * max_amplitude;
+
+    for (int n = 0; n < subchunk2Size / channelCount; ++n) {
+        double amplitude = (double) n / (subchunk2Size / channelCount) * max_amplitude;
         double value = sin((two_pi * n * frequency) / hz);
 
-        int i1 = (int) (max_amplitude - amplitude * value);
-        const char *s1 = reinterpret_cast<const char *>(&i1);
-        s1 = s1 != nullptr ? s1 : "";
+        { // clean line
+            int i1 = (int) (amplitude * value);
+            const char *s1 = reinterpret_cast<const char *>(&i1);
+            s1 = s1 != nullptr ? s1 : "";
+            file.write(s1, sizeof(int));
+        }
 
-        file.write(s1, sizeof(int));
+        { // clean line
+            int i1 = (int) (max_amplitude - amplitude * value);
+            const char *s1 = reinterpret_cast<const char *>(&i1);
+            s1 = s1 != nullptr ? s1 : "";
+            file.write(s1, sizeof(int));
+        }
+
     }
 
     file.close();
